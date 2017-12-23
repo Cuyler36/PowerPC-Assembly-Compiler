8124d8e20bb7ebc99519ecb8879bb71babfe9819
Cuyler36 <jeremy96@vt.edu>
2017-12-23 15:22:43 -0500

    Initial Commit

--- PPC Compiler/Form1.Designer.cs
+++ PPC Compiler/Form1.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.editor = new ScintillaNET.Scintilla();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buildToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // editor
+            // 
+            this.editor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editor.Location = new System.Drawing.Point(0, 0);
+            this.editor.Name = "editor";
+            this.editor.Size = new System.Drawing.Size(520, 460);
+            this.editor.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(639, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.newToolStripMenuItem,
+            this.buildToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // buildToolStripMenuItem
+            // 
+            this.buildToolStripMenuItem.Name = "buildToolStripMenuItem";
+            this.buildToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.buildToolStripMenuItem.Text = "Build";
+            this.buildToolStripMenuItem.Click += new System.EventHandler(this.buildToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.editor);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.outputTextBox);
+            this.splitContainer1.Size = new System.Drawing.Size(639, 460);
+            this.splitContainer1.SplitterDistance = 520;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.outputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(115, 460);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(639, 484);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "PowerPC Assembly Compiler";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ScintillaNET.Scintilla editor;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buildToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox outputTextBox;
     }
 }
 
--- PPC Compiler/Form1.cs
+++ PPC Compiler/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutocompleteMenuNS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,211 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace PPC_Compiler
 {
     public partial class Form1 : Form
     {
+        AutocompleteMenu InstructionMenu = new AutocompleteMenu();
+        AutocompleteMenu RegisterMenu = new AutocompleteMenu();
+        OpenFileDialog OpenDialog = new OpenFileDialog();
+        SaveFileDialog SaveDialog = new SaveFileDialog();
+
+        string[] Registers = {
+            "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "r16", "r17", "r18", "r19", "r20", "r21",
+            "r22", "r23", "r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31"
+        };
+
+        string[] Instructions = {
+            "li", "lis", "add", "addi", "mr", "lbz", "lbzx", "lhz", "lhzx", "lwz", "lwzx", "stb", "sth", "stw", "b", "bl", "bctr", "beq", "bne", "ble", "bge", "blt", "bgt",
+            "bdnz", "rlwinm", "addis", "cmpwi", "cmplwi", "cmpw", "cmplw", "mtctr", "and", "or", "andi", "ori", "divwu", "mulli", "srawi", "subf", "subis", "mtlr", "blr",
+            "bctrl", "bcctr", "add.", "rlwinm.", "rlwimi", "addo", "addo.", "addc", "addc.", "addco", "addco.", "adde", "adde.", "addeo", "addeo.", "addic", "addic.", 
+            "addme", "addme.", "addmeo", "addmeo.", "addze", "addze.", "addzeo", "addzeo.", "and.", "andc", "andc.", "andi.", "andis", "andis.", "ba", "bla", "bclr",
+            "bclrl", "cmp", "cmpi", "cmpl", "cmpli", "cntlzw", "cntlzw.", "crand", "crandc", "creqv", "crnand", "crnor", "cror", "crorc", "crxor", "stwu", "mflr",
+        };
+
         public Form1()
         {
             InitializeComponent();
+            editor.Margins[0].Type = ScintillaNET.MarginType.Number;
+            editor.Margins[0].Width = 28;
+            BuildInstructionAutocompleteMenu();
+            InstructionMenu.TargetControlWrapper = new ScintillaWrapper(editor);
+            RegisterMenu.TargetControlWrapper = new ScintillaWrapper(editor);
+
+            // Construct Custom Lexer
+            editor.Lexer = ScintillaNET.Lexer.Cpp;
+            editor.StyleResetDefault();
+            editor.Styles[ScintillaNET.Style.Default].Font = "Consolas";
+            editor.Styles[ScintillaNET.Style.Default].Size = 10;
+            editor.Styles[ScintillaNET.Style.Default].BackColor = Color.DarkGray;
+            editor.Styles[ScintillaNET.Style.Default].BackColor = Color.White;
+            editor.StyleClearAll();
+
+            editor.Styles[ScintillaNET.Style.Cpp.Default].BackColor = Color.DarkGray;
+            editor.Styles[ScintillaNET.Style.Cpp.Default].BackColor = Color.White;
+            editor.Styles[ScintillaNET.Style.Cpp.Word].ForeColor = Color.Blue;
+            editor.Styles[ScintillaNET.Style.Cpp.Word2].ForeColor = Color.LightSeaGreen;
+            editor.Styles[ScintillaNET.Style.Cpp.Number].ForeColor = Color.Olive;
+            editor.Styles[ScintillaNET.Style.Cpp.String].ForeColor = Color.FromArgb(163, 21, 21);
+            editor.Styles[ScintillaNET.Style.Cpp.Character].ForeColor = Color.FromArgb(163, 21, 21);
+
+            string Keywords = "";
+            foreach (string Instruction in Instructions)
+                Keywords += Instruction + " ";
+
+            string RegisterKeywords = "";
+            foreach (string Register in Registers)
+                RegisterKeywords += Register + " ";
+
+            editor.SetKeywords(0, Keywords);
+            editor.SetKeywords(1, RegisterKeywords);
+
+            OpenDialog.Filter = "PowerPC Assembly Source Files|*.pas|Text Files|*.txt|All Files|*.*";
+            OpenDialog.DefaultExt = "PowerPC Assembly Source Files|*.pas";
+            OpenDialog.FileName = "";
+
+            SaveDialog.Filter = "PowerPC Assembly Source Files|*.pas|Text Files|*.txt|All Files|*.*";
+            SaveDialog.DefaultExt = "PowerPC Assembly Source Files|*.pas";
+            SaveDialog.FileName = "";
+        }
+
+        private void BuildInstructionAutocompleteMenu()
+        {
+            var InstructionItems = new List<AutocompleteItem>();
+            
+            foreach (var Item in Instructions)
+            {
+                InstructionItems.Add(new SnippetAutocompleteItem(Item) { ImageIndex = 1 });
+            }
+
+            InstructionMenu.SetAutocompleteItems(InstructionItems);
+        }
+
+        private void BuildRegisterAutocompleteMenu()
+        {
+            var RegisterItems = new List<AutocompleteItem>();
+
+            foreach (var Item in Registers)
+            {
+                RegisterItems.Add(new SnippetAutocompleteItem(Item) { ImageIndex = 1 });
+            }
+
+            RegisterMenu.SetAutocompleteItems(RegisterItems);
+        }
+
+        private string GetConvertedAssembly()
+        {
+            var Path = Application.StartupPath;
+            if (File.Exists(Path + "\\powerpc-eabi-elf-as.exe") && File.Exists(Path + "\\powerpc-eabi-elf-objcopy.exe"))
+            {
+                var PPC_Compiler_Process = new Process();
+                PPC_Compiler_Process.StartInfo.FileName = Path + "\\powerpc-eabi-elf-as.exe";
+
+                var TextFile = File.CreateText(Path + "\\in.txt");
+                TextFile.Write(editor.Text);
+                TextFile.Flush();
+                TextFile.Close();
+
+                PPC_Compiler_Process.StartInfo.Arguments = "-mregnames -o out.o in.txt";
+                PPC_Compiler_Process.StartInfo.UseShellExecute = false;
+                PPC_Compiler_Process.StartInfo.CreateNoWindow = true;
+                PPC_Compiler_Process.Start();
+
+                PPC_Compiler_Process.WaitForExit();
+
+                if (PPC_Compiler_Process.ExitCode != 0)
+                    Console.WriteLine("powerpc-eabi-elf-as.exe exited with: " + PPC_Compiler_Process.ExitCode);
+
+                var PPC_Binary_Process = new Process();
+                PPC_Binary_Process.StartInfo.FileName = Path + "\\powerpc-eabi-elf-objcopy.exe";
+                PPC_Binary_Process.StartInfo.Arguments = "-O binary out.o out.bin";
+                PPC_Binary_Process.StartInfo.UseShellExecute = false;
+                PPC_Binary_Process.StartInfo.CreateNoWindow = true;
+                PPC_Binary_Process.Start();
+
+                PPC_Binary_Process.WaitForExit();
+
+                if (File.Exists(Path + "\\in.txt"))
+                {
+                    File.Delete(Path + "\\in.txt");
+                }
+
+                if (File.Exists(Path + "\\out.o"))
+                {
+                    File.Delete(Path + "\\out.o");
+                }
+
+                if (File.Exists(Path + "\\out.bin"))
+                {
+                    try
+                    {
+                        string PPCHexAssemblyString = "";
+                        var Data = File.ReadAllBytes(Path + "\\out.bin");
+                        File.Delete(Path + "\\out.bin");
+
+                        int[] ConvertedData = new int[Data.Length / 4];
+                        for (int i = 0; i < ConvertedData.Length; i++)
+                        {
+                            int idx = i * 4;
+                            ConvertedData[i] = (Data[idx] << 24) | (Data[idx + 1] << 16) | (Data[idx + 2] << 8) | Data[idx + 3];
+                        }
+
+                        for(int i = 0; i < ConvertedData.Length; i++)
+                        {
+                            PPCHexAssemblyString += ConvertedData[i].ToString("X8") + "\r\n";
+                        }
+
+                        Debug.WriteLine(PPCHexAssemblyString);
+
+                        return PPCHexAssemblyString;
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            return "";
+        }
+
+        private void buildToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var PPCString = GetConvertedAssembly();
+            outputTextBox.Text = PPCString;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OpenDialog.ShowDialog() == DialogResult.OK && File.Exists(OpenDialog.FileName))
+            {
+                editor.Text = File.ReadAllText(OpenDialog.FileName);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (SaveDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(SaveDialog.FileName))
+                {
+                    File.Delete(SaveDialog.FileName);
+                }
+
+                using (var Writer = new BinaryWriter(new FileStream(SaveDialog.FileName, FileMode.OpenOrCreate)))
+                {
+                    Writer.Write(Encoding.ASCII.GetBytes(editor.Text));
+                    Writer.Flush();
+                    Writer.Close();
+                }
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            editor.Text = ".text\n.org 0\n.globl _start\n\n_start:\nblr";
         }
     }
 }
